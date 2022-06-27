f6edcf4ce45760b4c220ff1ebf2507581727cf96
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-27 15:37:25 +0200

    4e commit - Exo 4

--- src/main/java/ex4/Pret.java
+++ src/main/java/ex4/Pret.java
@@ -5,36 +5,51 @@ import java.util.Date;
 public class Pret {
 
 	private Strategy strategy;
-	private double capital;
+	//private double capital;
 	private double capitalRestantDu;
-	private double taux;
-	private Date dateDebut;
+	//private double taux;
+	//private Date dateDebut;
 	private Date dateFin;
 	
+	/**
+	 * Constructeur à 3 paramètres. 
+	 * 
+	 * @param capital
+	 * @param taux
+	 * @param dateDebut
+	 */
 	public Pret(double capital, double taux, Date dateDebut) {
 		super();
-		this.strategy = new PretATerme();
-		this.capital = capital;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
+		this.strategy = new PretATerme(capital, taux, dateDebut);
 	}
 	
+	/**
+	 * Constructeur à 4 paramètres. 
+	 * 
+	 * @param capital
+	 * @param taux
+	 * @param dateDebut
+	 * @param dateFin
+	 */
 	public Pret(double capital, double taux, Date dateDebut, Date dateFin) {
 		super();
-		this.strategy = new PretATerme();
-		this.capital = capital;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
+		this.strategy = new PretATerme(capital, taux, dateDebut);
 		this.dateFin = dateFin;
 	}
 	
+	/**
+	 * Constructeur à 5 paramètres. 
+	 * 
+	 * @param capital
+	 * @param capitalRestantDu
+	 * @param taux
+	 * @param dateDebut
+	 * @param dateFin
+	 */
 	public Pret(double capital, double capitalRestantDu, double taux, Date dateDebut, Date dateFin) {
 		super();
-		this.strategy = new PretATerme();
-		this.capital = capital;
+		this.strategy = new PretATerme(capital, taux, dateDebut);
 		this.capitalRestantDu = capitalRestantDu;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 	}
 
@@ -52,20 +67,6 @@ public class Pret {
 		this.strategy = strategy;
 	}
 
-	/** Getter for capital
-	 * @return the capital
-	 */
-	public double getCapital() {
-		return capital;
-	}
-
-	/** Setter
-	 * @param capital the capital to set
-	 */
-	public void setCapital(double capital) {
-		this.capital = capital;
-	}
-
 	/** Getter for capitalRestantDu
 	 * @return the capitalRestantDu
 	 */
@@ -78,34 +79,6 @@ public class Pret {
 	 */
 	public void setCapitalRestantDu(double capitalRestantDu) {
 		this.capitalRestantDu = capitalRestantDu;
-	}
-
-	/** Getter for taux
-	 * @return the taux
-	 */
-	public double getTaux() {
-		return taux;
-	}
-
-	/** Setter
-	 * @param taux the taux to set
-	 */
-	public void setTaux(double taux) {
-		this.taux = taux;
-	}
-
-	/** Getter for dateDebut
-	 * @return the dateDebut
-	 */
-	public Date getDateDebut() {
-		return dateDebut;
-	}
-
-	/** Setter
-	 * @param dateDebut the dateDebut to set
-	 */
-	public void setDateDebut(Date dateDebut) {
-		this.dateDebut = dateDebut;
 	}
 
 	/** Getter for dateFin
--- src/main/java/ex4/PretATerme.java
+++ src/main/java/ex4/PretATerme.java
@@ -1,5 +1,74 @@
 package ex4;
 
+import java.util.Date;
+
 public class PretATerme implements Strategy {
 
+	private double capital;
+	private double taux;
+	private Date dateDebut;
+	
+	
+	/**
+	 * Constructeur;
+	 * 
+	 * @param capital
+	 * @param taux
+	 * @param dateDebut
+	 */
+	public PretATerme(double capital, double taux, Date dateDebut) {
+		super();
+		this.capital = capital;
+		this.taux = taux;
+		this.dateDebut = dateDebut;
+	}
+
+
+	/**
+	 * @return the capital
+	 */
+	public double getCapital() {
+		return capital;
+	}
+
+
+	/**
+	 * @param capital the capital to set
+	 */
+	public void setCapital(double capital) {
+		this.capital = capital;
+	}
+
+
+	/**
+	 * @return the taux
+	 */
+	public double getTaux() {
+		return taux;
+	}
+
+
+	/**
+	 * @param taux the taux to set
+	 */
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+
+
+	/**
+	 * @return the dateDebut
+	 */
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+
+	/**
+	 * @param dateDebut the dateDebut to set
+	 */
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	
 }
